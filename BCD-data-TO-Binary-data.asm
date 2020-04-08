721d3bbd8b20f0dd0dc125864753e7fc232480e9
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-08 12:58:37 +0530

    Add files via upload

---
+++ BCD-data-TO-Binary-data.asm
@@ -0,0 +1,27 @@
+ASSUME    CS:CODE, DS:DATA
+          DATA SEGMENT 
+	      BCD_NUM EQU 4576H
+	      BIN_NUM DW
+	      DATA ENDS
+	      CODE SEGMENTS
+START:    MOV AX, DATA          ; initializing data segment
+          MOV DS, AX
+	      MOV BX, BCD_NUM       ; BX is now having BCD number
+	      MOV CX, 0             ; initialization
+CONTINUE: CMP BX, 0             ; comparison to check BCD num is zero
+          JZ ENDPROG            ; if zero end the program 
+		  MOV AL, BL            ; 8 LSBs of number is transfered to AL
+		  SUM AL, 1             ; substract one from AL
+		  DAS                   ; decimal adjust after subtraction
+		  MOV BL, AL            ; result is stored in BL
+		  MOV AL, BH            ; 8 MSBs of number is transfered to AL
+		  SBB AL, 00H           ; subtraction with borrow
+		  DAS                   ; decimal adjust after subtraction
+		  MOV BH, AL            ; result back in BH register
+		  INC CX                ; increment CX by 1
+		  JMP CONTINUE
+ENDPROG:  MOV BIN_NUM, CX       ; result is stored in data segment
+          MOV AH, 4CH           ; termination of program
+          INT 21H               ; termination of program
+          CODE ENDS
+          END START		  
