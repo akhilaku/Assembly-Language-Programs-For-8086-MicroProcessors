1176b25c347b590370e9f6bbabd055d79b0af667
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-22 19:23:52 +0530

    Update To-Find-Largest Number in a Given Array.asm

--- To-Find-Largest Number in a Given Array.asm
+++ To-Find-Largest Number in a Given Array.asm
@@ -1,1 +1,12 @@
-
+ASSUME                      CS:CODE, DS:DATA
+DATA SEGMENT                LIST DB 52H, 23H, 56H, 45H
+                            COUNT EQU OF
+                            LARGEST DB 01H DUP(?)
+DATA ENDS
+CODE SEGMENT                
+START:                      MOV AX, DATA
+                            MOV DS, AX
+                            MOV SI, OFFSET LIST
+                            MOV CL, COUNT
+                            MOV AL, [SI]
+                            
