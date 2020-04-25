58a00afa4e8a82eb4b65be61957549d06c7546a6
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-25 12:09:23 +0530

    Update Exercise-1.asm

--- Exercise-1.asm
+++ Exercise-1.asm
@@ -3,3 +3,10 @@ MOV BL, 100           ; store the decimal value 100 (or 64H ) in BL.
 DIV BL                ; divide AX by BL to find the number of hundreds in the binary number.
 
 MOV DL, AL            ; move the quotient in AL (number of hundreds) to DL.
+MOV AL, AH            ; move the remainder in AH to AL.
+MOV AH, 00            ; Clear AH.
+MOV BL, 10            ; Store the decimal value 10( or 0AH) in BL.
+DIV BL                ; Divide AX by BL to find the number of tens in the binary number AH.
+                      ; Has the remainder, which is the number of ones in the binary number.
+                      
+
