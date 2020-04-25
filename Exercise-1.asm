17c3a4b291b97744446a505467d7f171df8c8529
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-25 12:06:22 +0530

    Update Exercise-1.asm

--- Exercise-1.asm
+++ Exercise-1.asm
@@ -1,1 +1,5 @@
+MOV AX, 00FFH         ; move the data FFH to AX, with the upper bytes as 00h.
+MOV BL, 100           ; store the decimal value 100 (or 64H ) in BL.
+DIV BL                ; divide AX by BL to find the number of hundreds in the binary number.
 
+MOV DL, AL            ; move the quotient in AL (number of hundreds) to DL.
