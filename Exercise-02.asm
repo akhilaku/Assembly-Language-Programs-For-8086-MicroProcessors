604f87d680b32896892a250393cb6f11e4c1ca18
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-29 22:38:33 +0530

    Update Exercise-02.asm

--- Exercise-02.asm
+++ Exercise-02.asm
@@ -1,1 +1,4 @@
+;Write a program to convert the given 8-bit binary number into ASCII codes.
+;The 8-bit binary number is present in the memory location 2000H:5000H and the result is to be stored at the offset addressess 1000H and 1001H in the same segment.
+
 
