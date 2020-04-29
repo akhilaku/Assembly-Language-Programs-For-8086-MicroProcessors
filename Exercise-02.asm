efd4e18232730cb9bc412f71b235a58c22fa136d
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-29 22:48:49 +0530

    Update Exercise-02.asm

--- Exercise-02.asm
+++ Exercise-02.asm
@@ -10,5 +10,16 @@ MOV CL, 04
 ROR AL, CL                            ; Rotate AL right four times, to get the upper nibble.
 CALL ASCII                            ; call the subroutine ASCII.
 MOV[1000H], AL                        ; store the result in AL in the memory.
+MOV AL, BL                            ; move the binary data again to AL.
+AND AL, 0FH                           ; mask the upper nibble in AL.
+CALL ASCII                            ; call the subroutine ASCII.
+MOV[1001H], AL                        ; store the result in AL in the memory.
+JMP L1                                ; jump to L1.
+ASCII: CMP AL, 0AH                    ; compares AL with the value 0AH.
+JC L2                                 ; if AL is lesser than 0AH, go to L2.
+ADD AL, 07H                           ; add 07H with AL.
+L2: ADD AL, 30H                       ; add 30H with AL.
+RET                                   ; return to the main program.
+L1: HLT                               ; stop.
 
 
