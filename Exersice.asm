6030d6d0840dc947836eef50724bfae7681e6a96
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-05-04 21:13:35 +0530

    Update Exersice.asm

--- Exersice.asm
+++ Exersice.asm
@@ -3,8 +3,13 @@
 ;Use 8-bit data and 1 stop bit.
 ;Use timer 1.
 
-SBUF1 EQU 0C1H             ; 2nd serial SBUF addr
-SCON1 EQU OCOH             ; 2nd serial SCON addr
-TI1 BIT 0C1H               ; 2nd serial TI bit addr
-RI1 BIT 0C0H               ; 2nd serial RI bit addr
-ORG 0H
+           SBUF1 EQU 0C1H             ; 2nd serial SBUF addr
+           SCON1 EQU OCOH             ; 2nd serial SCON addr
+           TI1 BIT 0C1H               ; 2nd serial TI bit addr
+           RI1 BIT 0C0H               ; 2nd serial RI bit addr
+           ORG 0H
+
+MAIN:      MOV TMOD,#20H              ; COM2 uses Timer 1 on reset
+           MOV TH1,#-6                ; 4800 baud rate
+           MOV SCON1,#50H             ; 8-bit, 1 stop, REN enabled
+     
