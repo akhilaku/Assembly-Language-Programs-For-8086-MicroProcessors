5c0e99dead8db92f4bb039d8d3b78bb643fd7fd4
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-05-04 21:11:22 +0530

    Update Exersice.asm

--- Exersice.asm
+++ Exersice.asm
@@ -2,3 +2,9 @@
 ;continuously transfer the letter "A" serially at 4800 baud.
 ;Use 8-bit data and 1 stop bit.
 ;Use timer 1.
+
+SBUF1 EQU 0C1H             ; 2nd serial SBUF addr
+SCON1 EQU OCOH             ; 2nd serial SCON addr
+TI1 BIT 0C1H               ; 2nd serial TI bit addr
+RI1 BIT 0C0H               ; 2nd serial RI bit addr
+ORG 0H
